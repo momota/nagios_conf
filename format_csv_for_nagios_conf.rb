c6ed7010f48f086436ab4c51ab188de2006d6fa1
momota <makoto.momota@gmail.com>
2014-06-14 08:18:29 +0000

    fix variables name from camelcase to snakecase. add @interface.  modify constant value.

--- format_csv_for_nagios_conf.rb
+++ format_csv_for_nagios_conf.rb
@@ -9,17 +9,21 @@ class FormatCSVForNagiosConf
   # TODO: yamlなどに外出しする
   W_ERROR_THRESHOLD  = 0
   C_ERROR_THRESHOLD  = 0
-  W_TRAFFIC_THRESHOLD_10G  = 7000000000
-  C_TRAFFIC_THRESHOLD_10G  = 9000000000
-  W_TRAFFIC_THRESHOLD_1G   = 700000000
-  C_TRAFFIC_THRESHOLD_1G   = 900000000
-  W_TRAFFIC_THRESHOLD_100M = 70000000
-  C_TRAFFIC_THRESHOLD_100M = 90000000
 
-  def initialize( inputFilePath )
-    @inputFilePath = inputFilePath
-    @hostname      = File::basename(@inputFilePath, '.*')
+  warn_rate  = 0.7
+  error_rate = 0.9
+  W_TRAFFIC_THRESHOLD_10G  = 10000000000 * warn_rate
+  C_TRAFFIC_THRESHOLD_10G  = 10000000000 * error_rate
+  W_TRAFFIC_THRESHOLD_1G   = 1000000000  * warn_rate
+  C_TRAFFIC_THRESHOLD_1G   = 1000000000  * error_rate
+  W_TRAFFIC_THRESHOLD_100M = 100000000   * warn_rate
+  C_TRAFFIC_THRESHOLD_100M = 100000000   * error_rate
 
+  def initialize( input_file_path )
+    @input_file_path = input_file_path
+    @hostname      = File::basename(@input_file_path, '.*')
+
+    @interface = ''
     @ifIndex   = ''
     @w_traffic_threshold = 0
     @c_traffic_threshold = 0
@@ -37,19 +41,18 @@ class FormatCSVForNagiosConf
     cfg_ifInErrors  = ''
     cfg_ifOutErrors = ''
 
-    row = CSV.read( @inputFilePath )
+    row = CSV.read( @input_file_path )
     row.each { |r|
-      case r[0]
+      @interface = r[0]
+      @ifIndex   = r[1]
+      case @interface
       when @judge_10G
-        @ifIndex = r[1]
         @w_traffic_threshold = W_TRAFFIC_THRESHOLD_10G
         @c_traffic_threshold = C_TRAFFIC_THRESHOLD_10G
       when @judge_1G
-        @ifIndex = r[1]
         @w_traffic_threshold = W_TRAFFIC_THRESHOLD_1G
         @c_traffic_threshold = C_TRAFFIC_THRESHOLD_1G
       when @judge_100M
-        @ifIndex = r[1]
         @w_traffic_threshold = W_TRAFFIC_THRESHOLD_100M
         @c_traffic_threshold = C_TRAFFIC_THRESHOLD_100M
       else
@@ -68,8 +71,8 @@ end
 
 # [JUNIPER] format csv file fir nagios conf
 class FormatCSVForNagiosConfJuniper < FormatCSVForNagiosConf
-  def initialize( inputFilePath )
-    super( inputFilePath )
+  def initialize( input_file_path )
+    super( input_file_path )
     @judge_10G  = Regexp.new('^xe\-')
     @judge_1G   = Regexp.new('^ge\-')
     @judge_100M = Regexp.new('^fe\-')
@@ -79,8 +82,8 @@ end
 
 # [CISCO] format csv file fir nagios conf
 class FormatCSVForNagiosConfCisco < FormatCSVForNagiosConf
-  def initialize( inputFilePath )
-    super( inputFilePath )
+  def initialize( input_file_path )
+    super( input_file_path )
     @judge_10G  = Regexp.new('^TenGigabitEthernet')
     @judge_1G   = Regexp.new('^GigabitEthernet')
     @judge_100M = Regexp.new('^FastEthernet')
